9b6174fb7b53ecb38cec0344d543cd75779b62a3
Rhuan Pablo <rhuanpablo13saga@gmail.com>
2021-10-17 22:47:19 -0300

    iniciando projeto

---
+++ src/main/java/com/dminer/dminer/DminerApplication.java
@@ -0,0 +1,13 @@
+package com.dminer.dminer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DminerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DminerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dminer/dminer/DminerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dminer.dminer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DminerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
