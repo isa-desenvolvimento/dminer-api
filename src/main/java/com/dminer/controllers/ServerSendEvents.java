132c675fe4807c9f957074e3c9d7859e44f04807
Rhuan <rhuanpablo13@hotmail.com>
2021-10-28 15:18:35 -0300

    server-sent-events

---
+++ src/main/java/com/dminer/controllers/ServerSendEvents.java
@@ -0,0 +1,39 @@
+package com.dminer.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.Duration;
+import java.time.LocalTime;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Flux;
+
+@RestController
+@RequestMapping("/server-send-events")
+@CrossOrigin(origins = "*")
+@RequiredArgsConstructor
+public class ServerSendEvents {
+    
+    @GetMapping(path = "/stream-flux", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> streamFlux() {
+        return Flux.interval(Duration.ofSeconds(1))
+        .map(sequence -> "Flux - " + LocalTime.now().toString());
+    }
+
+}
