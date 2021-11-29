04785382b0144f025be04c267cba6dc499533a5e
Rhuan <rhuanpablo13@hotmail.com>
2021-11-29 10:10:52 -0300

    comment controller

--- src/main/java/com/dminer/controllers/CommentController.java
+++ src/main/java/com/dminer/controllers/CommentController.java
@@ -21,9 +21,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/comment")
+@CrossOrigin(origins = "*")
+@RequiredArgsConstructor
 public class CommentController {
     
 
