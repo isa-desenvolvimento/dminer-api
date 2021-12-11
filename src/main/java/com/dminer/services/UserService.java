699db0a3bda1412369f3bb9056d763469a965ed6
Rhuan <rhuanpablo13@hotmail.com>
2021-12-11 19:35:00 -0300

    search

--- src/main/java/com/dminer/services/UserService.java
+++ src/main/java/com/dminer/services/UserService.java
@@ -281,24 +281,10 @@ public class UserService implements IUserService {
     
     
     public String getAvatarBase64ByLogin(String login) {
-    	try {
-    		
-    		String dir = this.getAvatarDir(login);
-    		if (dir == null) {
-    			BufferedImage image = ImageIO.read(new URL("https://www.dminerweb.com.br:8553/api/auth/avatar/?login_user=" + login));
-    			if (image != null) {
-    				ByteArrayOutputStream baos = new ByteArrayOutputStream();
-    				ImageIO.write(image, "png", baos);
-    				byte[] bytes = baos.toByteArray();
-    				String base64AsString = "data:image/png;base64," + new String(org.bouncycastle.util.encoders.Base64.encode(bytes));
-        			System.out.println(base64AsString);
-        			return base64AsString;
-    			} else {
-    				return this.getAvatarBase64(dir);
-    			}
-    		}
-    		
-    	} catch (IOException e) {}
+		String dir = this.getAvatarDir(login);
+		if (dir != null) {
+			return this.getAvatarBase64(dir);
+		}
     	return null;
     }
     
