0c1f1b83407dee52199fdb1360a34211bc435e4e
Rhuan <rhuanpablo13@hotmail.com>
2022-02-22 22:17:42 -0300

    ajustes

--- src/main/java/com/dminer/controllers/NotificationController.java
+++ src/main/java/com/dminer/controllers/NotificationController.java
@@ -192,22 +192,22 @@ public class NotificationController {
         return ResponseEntity.ok().body(response);
     }
 
-    @GetMapping(value = "/search/{login}/{keyword}")
-    @Transactional(timeout = 90000)
-    public ResponseEntity<Response<List<NoticeDTO>>> search(@RequestHeader("x-access-token") Token token, @PathVariable String login, @PathVariable String keyword) {
+    // @GetMapping(value = "/search/{login}/{keyword}")
+    // @Transactional(timeout = 90000)
+    // public ResponseEntity<Response<List<NoticeDTO>>> search(@RequestHeader("x-access-token") Token token, @PathVariable String login, @PathVariable String keyword) {
         
-        Response<List<NoticeDTO>> response = new Response<>();
+    //     Response<List<NoticeDTO>> response = new Response<>();
 
-        List<Notice> search = noticeService.search(keyword, isProd());
-        search.forEach(notice -> {
-            NoticeDTO dto = noticeConverter.entityToDTO(notice);
-            response.getData().add(dto); 
-        });
+    //     List<Notice> search = noticeService.search(keyword, isProd());
+    //     search.forEach(notice -> {
+    //         NoticeDTO dto = noticeConverter.entityToDTO(notice);
+    //         response.getData().add(dto); 
+    //     });
         
-        return ResponseEntity.ok().body(response);
-    }
+    //     return ResponseEntity.ok().body(response);
+    // }
 
-    public boolean isProd() {
-        return Arrays.asList(env.getActiveProfiles()).contains("prod");
-    }
+    // public boolean isProd() {
+    //     return Arrays.asList(env.getActiveProfiles()).contains("prod");
+    // }
 }
